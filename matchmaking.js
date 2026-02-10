62259c0f89cf92e704684a4ac519bbfd54d23c4c
Samir000-maker <mdmaksudulhasansohan@gmail.com>
2026-02-11 00:53:28 +0600

    Update matchmaking.js

--- matchmaking.js
+++ matchmaking.js
@@ -5,7 +5,7 @@ let redis = null;
 let io = null;
 
 const ROOM_LIFETIME = 600000; // 10 minutes
-const ROOM_WARNING_TIME = 60000; // 1 minute
+const ROOM_WARNING_TIME = 600000; // 1 minute
 
 /**
  * Initialize matchmaking with Redis client and Socket.IO
