2b9dd0df04b71156d99a0042f80498184ebd461b
Samir000-maker <mdmaksudulhasansohan@gmail.com>
2026-02-12 20:49:25 +0600

    Update server.js

--- server.js
+++ server.js
@@ -1673,7 +1673,7 @@ const io = new Server(server, {
 
 // Initialize Redis-backed matchmaking
 matchmaking.init(pubClient, io, redlock);
-
+app.use(express.static(__dirname + '/public'));
 app.use(cors());
 app.use(express.json({ limit: '100mb' }));
 app.use(express.urlencoded({ limit: '100mb', extended: true }));
