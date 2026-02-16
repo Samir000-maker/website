6f524decbdf22ba9682056d885fbb701cadee4f5
Samir000-maker <mdmaksudulhasansohan@gmail.com>
2026-02-17 01:00:32 +0600

    Update config.js

--- config.js
+++ config.js
@@ -24,7 +24,7 @@ export const config = {
   PROFILE_CACHE_TTL_SECONDS: 86400, // 24 hours
 
   // Matchmaking configuration
-  MATCHMAKING_TIMEOUT: 10000, // 5 seconds - global configurable search timeout
+  MATCHMAKING_TIMEOUT: 30000, 
   MIN_USERS_FOR_ROOM: 2, // Minimum users required to create a room
 
   // File upload limits
