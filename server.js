9eae8bc781958d3dffe3f978a013d8329609503f
Samir000-maker <mdmaksudulhasansohan@gmail.com>
2026-02-20 00:59:31 +0600

    Update server.js

--- server.js
+++ server.js
@@ -2130,6 +2130,14 @@ const io = new Server(server, {
 matchmaking.init(pubClient, io, redlock);
 
 app.use(cors());
+
+app.use((req, res, next) => {
+  try {
+    res.setHeader('Cross-Origin-Opener-Policy', 'same-origin-allow-popups');
+  } catch { }
+  next();
+});
+
 app.use(express.json({ limit: '100mb' }));
 app.use(express.urlencoded({ limit: '100mb', extended: true }));
 
