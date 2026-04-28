4dbe4ee141d542c24ea2875728c99fd4a636a929
Samir000-maker <mdmaksudulhasansohan@gmail.com>
2026-04-29 02:12:14 +0600

    Update server.js

--- server.js
+++ server.js
@@ -65,7 +65,7 @@ const redisPassword = config.REDIS_PASSWORD || 'uSwjFF7U0DPoQL6o7OqlafrM0zwNpPAm
 
 // Redis connection URL
 // const redisUrl = `redis://:${encodeURIComponent(redisPassword)}@${redisHost}:${redisPort}`;
-const redisUrl = `rediss://default:${encodeURIComponent(redisPassword)}@${redisHost}:${redisPort}`;
+const redisUrl = `rediss://default:${encodeURIComponent(redisPassword)}@TLS_HOST:TLS_PORT`;
 
 
 const pubClient = new Redis(redisUrl);
